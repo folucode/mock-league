6f6a42167d412ddbeca32a8ca5a5db5aca81d35c
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-09-18 15:31:01 +0100

    🔒 set up auth middleware

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middlewares/auth')
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
