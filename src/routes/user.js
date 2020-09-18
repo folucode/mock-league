781ae804d79d169af18e802842a1fdb1ced61544
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-09-18 19:39:27 +0100

    ✨ Add user update profile

--- src/routes/user.js
+++ src/routes/user.js
@@ -32,4 +32,32 @@ router.get('/users/me', auth, async (req, res) => {
 	res.send(req.user);
 });
 
+router.patch('/users/me', auth, async (req, res) => {
+	const { user, body } = req;
+
+	const updates = Object.keys(body);
+	const allowedUpdates = ['name', 'email', 'password'];
+	const isValidOperation = updates.every((update) =>
+		allowedUpdates.includes(update),
+	);
+
+	if (!isValidOperation) {
+		return res.status(400).send({ error: 'Invalid updates!' });
+	}
+
+	try {
+		updates.forEach((update) => (user[update] = body[update]));
+
+		await user.save();
+
+		if (!user) {
+			return res.status(404).send();
+		}
+
+		res.send(req.user);
+	} catch (error) {
+		res.status(400).send(error.message);
+	}
+});
+
 module.exports = router;
