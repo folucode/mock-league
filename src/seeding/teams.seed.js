93741ead9524e7c2f5df99789b92e3a820094449
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-09-29 19:05:08 +0100

    🌱 update teams seed file

--- src/seeding/teams.seed.js
+++ src/seeding/teams.seed.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Team = require('../models/team');
 const router = new express.Router();
 const faker = require('faker');
+const { v4: uuidv4 } = require('uuid');
 
 const algoliaclient = require('../search/algolia');
 
@@ -12,29 +13,28 @@ router.post('/teams/seed', async (req, res) => {
 		let teams = [];
 
 		for (let index = 0; index < 100; index++) {
-
-			
-
 			let fullname = faker.lorem.words(2);
 			let short_name = `${fullname.charAt(0).toUpperCase()}FC`;
+			let objectID = uuidv4();
 
 			let newTeam = {
 				fullname,
 				nickname: faker.lorem.slug(),
 				short_name,
 				founded: Math.floor(faker.finance.amount(1800, 2020)),
+				objectID,
 			};
 
 			teams.push(newTeam);
 		}
 
+		await Team.insertMany(teams);
+
 		await teamIndex.saveObjects(teams, {
 			autoGenerateObjectIDIfNotExist: true,
 		});
 
-		await Team.insertMany(teams);
-
-		res.send('Data seeding successful');
+		res.send({ response: 'Data seeding successful', teams });
 	} catch (error) {
 		res.status(400).send(error.message);
 	}
