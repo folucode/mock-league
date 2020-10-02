7204b97c57ee55b35773aac2f6390a6f27e89afd
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-10-02 17:20:13 +0100

    ✨ Setup redis

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-const express = require('express');
-require('./db/mongoose');
-const userRoutes = require('./routes/user');
-const teamRoutes = require('./routes/team');
-const fixturesRoutes = require('./routes/fixture');
-const userSeeding = require('./seeding/users.seed');
-const teamSeeding = require('./seeding/teams.seed');
-const fixtureSeeding = require('./seeding/fixtures.seed');
+const express = require("express");
+require("./db/mongoose");
+const redis = require("redis");
+const userRoutes = require("./routes/user");
+const teamRoutes = require("./routes/team");
+const fixturesRoutes = require("./routes/fixture");
+const userSeeding = require("./seeding/users.seed");
+const teamSeeding = require("./seeding/teams.seed");
+const fixtureSeeding = require("./seeding/fixtures.seed");
 
-const searchRoute = require('./routes/search');
+const searchRoute = require("./routes/search");
 
 const app = express();
+const client = redis.createClient(6379, "localhost");
 
 app.use(express.json());
 app.use(userRoutes);
@@ -21,4 +23,7 @@ app.use(userSeeding);
 app.use(teamSeeding);
 app.use(fixtureSeeding);
 
-module.exports = app;
+module.exports = {
+  app,
+  client
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-const app = require('./app');
+const { app } = require("./app");
+const { client } = require("./app");
 
 const port = process.env.PORT;
 
+client.on("connect", function () {
+  console.log(`Redis Client running`);
+});
+
 app.listen(port, () => {
-	console.log(`Server up on ${port}`);
+  console.log(`Server up on ${port}`);
 });
