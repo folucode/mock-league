2d6416325974d7e04d4b05b99c086804e370de22
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-10-14 17:24:28 +0100

    ✨Implement rate-limiting to all endpoints

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const fixturesRoutes = require("./routes/fixture");
 const userSeeding = require("./seeding/users.seed");
 const teamSeeding = require("./seeding/teams.seed");
 const fixtureSeeding = require("./seeding/fixtures.seed");
+const rateLimit = require("express-rate-limit");
 
 const { v4: uuid_v4 } = require("uuid");
 
@@ -17,7 +18,12 @@ const redisClient = require("./db/redis");
 const app = express();
 
 app.use(express.json());
-
+ 
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, 
+  max: 100
+});
+ 
 // app.use(
 //   session({
 //     genid: (req) => {
@@ -34,6 +40,7 @@ app.use(express.json());
 //     resave: false,
 //   })
 // );
+app.use(limiter);
 
 app.use(userRoutes);
 app.use(teamRoutes);
