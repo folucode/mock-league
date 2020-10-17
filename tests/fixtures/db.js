1db2be1a2b8865b1fa815ba18e744b32889c085f
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-10-17 11:57:45 +0100

    🧪 update tests config

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../../src/models/user");
 const { v4: uuid_v4 } = require("uuid");
 const Fixture = require("../../src/models/fixture");
+const Team = require("../../src/models/team");
 
 const id_one = new mongoose.Types.ObjectId();
 const regularUser = {
@@ -45,12 +46,24 @@ const fixture = {
   link: `/fixtures/Congo-United-v-Atlas-FC/${fixture_id}`,
 };
 
+const id_four = new mongoose.Types.ObjectId();
+const team = {
+  _id: id_four,
+  fullname: "Crimon FC",
+  nickname: "Tigers",
+  short_name: "CFC",
+  objectID: uuid_v4(),
+  founded: 1970,
+};
+
 const setupData = async () => {
   await User.deleteMany();
   await Fixture.deleteMany();
+  await Team.deleteMany();
   await new User(regularUser).save();
   await new User(adminUser).save();
   await new Fixture(fixture).save();
+  await new Team(team).save();
 };
 
 module.exports = {
@@ -61,5 +74,6 @@ module.exports = {
   setupData,
   fixture,
   id_three,
+  id_four,
   fixture_id,
 };
